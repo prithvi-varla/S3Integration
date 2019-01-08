9c6a1b0e2f33cd391bf8450ac816f70e673fb42a
prithvirajv <prithviraj.varla@concur.com>
2019-01-07 20:27:10 -0600

    jenkins file commit (travis ci- test build6)

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,14 @@
-# Use this for Docker Hub
-docker login --username $DOCKER_HUB_USER --password $DOCKER_HUB_PSW
+pip install --user awscli
+export PATH=$PATH:$HOME/.local/bin
 
-docker build -t hub.docker.com/prithvi425/s3integration .
-docker tag hub.docker.com/prithvi425/s3integration:latest prithvi425/s3integration:latest
-docker push prithvi425/s3integration:latest
+add-apt-repository ppa:eugenesan/ppa
+apt-get update
+apt-get install jq -y
+
+curl https://raw.githubusercontent.com/silinternational/ecs-deploy/master/ecs-deploy | sudo tee -a /usr/bin/ecs-deploy
+sudo chmod +x /usr/bin/ecs-deploy
+
+# Use this for AWS ECR
+# eval $(aws ecr get-login --region us-west-2)
 
 ecs-deploy -c secondCluster -n secondService -i prithvi425/s3integration:latest
